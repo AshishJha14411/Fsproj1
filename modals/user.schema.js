7cf3f5254fc5ec91c334387ffd9668cac62a9d2c
Ashish Jha <ashishjha14411@gmail.com>
2022-12-04 10:04:14 +0530

    added forgot pass

--- modals/user.schema.js
+++ modals/user.schema.js
@@ -1,62 +1,58 @@
-import mongoose, { mongo } from "mongoose";
-import AuthRoles from "../utils/authRoles";
-import bcrpyt from'bcryptjs'
-import JWT from 'jsonwebtoken'
+import mongoose from "mongoose";
+import AuthRoles from '../utils/authRoles'
+import bcrypt from "bcryptjs"
+import JWT from "jsonwebtoken"
 import crypto from "crypto"
-import config from "../config/index";
+import config from "../config/index"
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is Required"],
-    maxLength: [25, "Name Must be less than 25 Character"],
-  },
-  //trim it donw, either front or backend
-  email: {
-    type: String,
-    required: [true, "Email is Required"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is Required"],
-    maxLength: [6, "Password must be atleast 6 Characters"],
-    //this will not select the password when we send the data back. so we wont have to assign it undefined when it is sent
-    select: false,
-  },
-  role: {
-    type: String,
-    //basic js. do revision if not understood
-    enum: Object.values(AuthRoles),
-    //assigning default role of user
-    default: AuthRoles.USER,
-  },
-  forgotPasswordToken: String,
-  forgotPasswordExpiry: Date,
-},
-{
-    //can be used to create timestaps 
-    timestamps: true
-}
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+            maxLength: [50, "Name must be less than 50"]
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true
+        },
+        password: {
+            type: String,
+            required: [true, "password is required"],
+            minLength: [8, "password must be at least 8 characters"],
+            select: false
+        },
+        role: {
+            type: String,
+            enum: Object.values(AuthRoles),
+            default: AuthRoles.USER
+        },
+        forgotPasswordToken: String,
+        forgotPasswordExpiry: Date,
+    },
+    {
+        timestamps: true
+    }
 );
 
-//challenge 1 - encrpty password
-
-userSchema.pre("save", async function(next){
-    if(!this.modified("password")) return next()
-    this.password = await bcrpyt.hash(this.password, 10)
+// challenge 1 - encrypt password - hooks
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
-//adding more features directly to Schema
-
+// add more featuers directly to your schema
 userSchema.methods = {
     //compare password
-    comparePassword: async function(enteredPassword){
-        return await bcrpyt.compare(enteredPassword, this.password)
+    comparePassword: async function (enteredPassword) {
+        return await bcrypt.compare(enteredPassword, this.password)
     },
-    //genrate JWT TOKEN
-    getJwtToken: function(){
+
+    //generate JWT TOKEN
+    getJwtToken: function () {
         return JWT.sign(
             {
                 _id: this._id,
@@ -66,13 +62,26 @@ userSchema.methods = {
             {
                 expiresIn: config.JWT_EXPIRY
             }
-            //bad practice of secret but a way to do it. Above code is better way and is cleaner
-            /* 'bad-secret',{
-                expiresIn: '2h-bad'
-            } */
-
         )
+    },
+
+    generateForgotPasswordToken: function(){
+        const forgotToken = crypto.randomBytes(20).toString('hex');
+
+        //step 1 - save to DB
+        this.forgotPasswordToken = crypto
+        .createHash("sha256")
+        .update(forgotToken)
+        .digest("hex")
+
+        this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000
+        //step 2 - return values to user
+
+        return forgotToken
+
     }
 }
+
+
 
 export default mongoose.model("User", userSchema)
