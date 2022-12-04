3449cac3c01f7322ef818dbd912ca6616d8d86c2
Ashish Jha <ashishjha14411@gmail.com>
2022-12-04 10:36:29 +0530

    pushing product schema

---
+++ modals/product.schema.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter a product name"],
+      trim: true,
+      maxLength: [120, "Product name should of max 120 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please enter a product product"],
+      trim: true,
+      maxLength: [5, "Product price should not exceed 5 digits"],
+    },
+    description:{
+        type: String,
+        //use some form of wsiyg editor- assignment
+    },
+    photos:[{
+        secure_url: {
+            type: String,
+            required: true
+        }
+    }],
+    stocks: {
+        type: Number,
+        default: 0
+    },
+    sold: {
+        type:Number,
+        default: 0
+    },
+    collectionId: {
+        //we need to have a collection linkage
+        //general form mongose.Schema.Types.ObjectID
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Collection"
+    }
+},
+  {
+    timestamps: true,
+  }
+);
